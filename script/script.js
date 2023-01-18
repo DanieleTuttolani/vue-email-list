b7288ae09dceb22ad079703cf02647eac77fe283
DanieleTuttolani <danieletuttolani2@gmail.com>
2023-01-18 15:13:03 +0100

    ticket problem

---
+++ script/script.js
@@ -0,0 +1,24 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            emails :[]
+        }
+    },
+    methods:{
+        getEmail(){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .this((response) =>{
+                this.emails.push(response.data.response)
+                
+            })
+        }
+    }
+    ,
+    mounted(){
+        for(let i = 0 ; i < 5 ; i++){
+            this.getEmail()
+        }
+    }
+})
+
+app.mount('#root')
